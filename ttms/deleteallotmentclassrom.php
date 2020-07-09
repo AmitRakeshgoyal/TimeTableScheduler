52a63fd6edd2f5fa6d525da5945a1c588b0e427a
Amit goyal <68068371+AmitRakeshgoyal@users.noreply.github.com>
2020-07-09 20:02:17 +0530

    Add files via upload

---
+++ ttms/deleteallotmentclassrom.php
@@ -0,0 +1,15 @@
+<?php
+
+include 'connection.php';
+$id = $_GET['name'];
+
+$q = mysqli_query(mysqli_connect("localhost", "root", "", "ttms"),
+    "UPDATE classrooms  SET status = '0' WHERE name = '$id' ");
+if ($q) {
+
+    header("Location:allotclasses.php");
+
+} else {
+    echo 'Error';
+}
+?>
